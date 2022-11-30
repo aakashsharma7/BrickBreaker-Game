c57baa7e1b0c41e48ad5a8e2c7d898dbd98408ad
Aakash Sharma <94851495+aakashsharma7@users.noreply.github.com>
2022-11-30 16:14:05 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package BrickBreakerMP;
+
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10, 10 , 700, 600);
+        obj.setTitle("BreakOut Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+   }
+}
